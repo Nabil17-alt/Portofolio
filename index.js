64a555c36716e6712b555f50b93cb57739835267
Muhammad Nabil <realmuhammadnabil@gmail.com>
2024-12-26 20:00:35 +0700

    add project

--- index.js
+++ index.js
@@ -1,87 +1,88 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.navbar');
-menu.onclick = () => {
-    navbar.classList.toggle("open-menu");
-    menu.classList.toggle("move");
-};
-window.onscroll = () =>{
-    navbar.classList.remove("open-menu");
-    menu.classList.remove("move");
-}
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
-const txtElement = ['Muhammad Nabil'];
+menu.onclick = () => {
+  navbar.classList.toggle("open-menu");
+  menu.classList.toggle("move");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("open-menu");
+  menu.classList.remove("move");
+};
+
+const txtElement = ["Muhammad Nabil"];
 let count = 0;
 let txtIndex = 0;
-let currentTxt ='';
-let words = '';
+let currentTxt = "";
+let words = "";
 
-(function ngetik(){
+(function ngetik() {
+  if (count == txtElement.length) {
+    count = 0;
+  }
 
-    if(count == txtElement.length){
-        count = 0;
-    }
+  currentTxt = txtElement[count];
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector(".efek-ngetik").textContent = words;
 
-    currentTxt = txtElement[count];
+  if (words.length == currentTxt.length) {
+    count++;
+    txtIndex = 0;
+  }
 
-    words = currentTxt.slice(0, ++txtIndex);
-    document.querySelector('.efek-ngetik').textContent = words;
-
-    if(words.length == currentTxt.length){
-        count++;
-        txtIndex = 0;
-    }
-
-    setTimeout(ngetik, 500);
-
+  setTimeout(ngetik, 500);
 })();
 
 function validate() {
-    let name = document.querySelector(".name");
-    let email = document.querySelector(".email");
-    let msg = document.querySelector(".message");
-    let sendBtn = document.querySelector(".send-btn");
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
 
-    sendBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (name.value == "" || email.value == "" || msg.value == "") {
-            emptyerror();
-        } else {
-            sendmail(name.value, email.value, msg.value);
-            success();
-        }
-    })
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
 }
+
 validate();
 
-function sendmail (name,email,msg){
-    emailjs.send("service_x5omr5j", "template_595xcxk", {
-      to_name: email,
-      from_name: name,
-      message: msg,
-    });
+function sendmail(name, email, msg) {
+  emailjs.send("service_x5omr5j", "template_595xcxk", {
+    to_name: email,
+    from_name: name,
+    message: msg,
+  });
 }
 
 function emptyerror() {
-    swal({
-        title: "Gagal Terkirim",
-        text: "Belum dikembangkan Nabil!",
-        icon: "error",
-      });
-}
-function success() {
-    swal({
-      title: "Gagal Terkirim",
-      text: "Belum dikembangkan Nabil!",
-      icon: "error",
-    });
+  swal({
+    title: "Gagal Terkirim",
+    text: "Belum dikembangkan Nabil!",
+    icon: "error",
+  });
 }
 
-let header = document.querySelector('header')
-window.addEventListener('scroll', () => {
-    header.classList.toggle('header-active', window.scrollY > 0);
+function success() {
+  swal({
+    title: "Gagal Terkirim",
+    text: "Belum dikembangkan Nabil!",
+    icon: "error",
+  });
+}
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
 });
 
-let scrollTop = document.querySelector('.scroll-top')
-window.addEventListener('scroll', () => {
-    scrollTop.classList.toggle('scroll-active', window.scrollY >= 400);
+let scrollTop = document.querySelector(".scroll-top");
+window.addEventListener("scroll", () => {
+  scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
 });
